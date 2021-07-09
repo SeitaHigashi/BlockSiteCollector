af7a6424859a70896fbbc6834f30cfcd0c945712
SeitaHigashi <higashi110902@gmail.com>
2021-07-09 11:47:07 +0900

    Add connectivity

--- src/connectivity.ts
+++ src/connectivity.ts
@@ -1,2 +1,30 @@
-class Connectivity {
+import {customsearch_v1} from "googleapis"
+import { Search } from "./search"
+global.fetch = require('node-fetch').default;
+
+export class Connectivity {
+  public static async try(url:string) {
+    try {
+      const response = await fetch(url);
+      return response.ok;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  public static tryResults(results:customsearch_v1.Schema$Result[]){
+    const promises = results.map(result => {
+      return this.try(result.link!);
+    })
+    Promise.all(promises).then(results => console.log(results));
+
+  }
 }
+
+Search.search("java sql")
+.then(result => Connectivity.tryResults(result.data.items!))
+.then(result => console.log(result));
+
+Search.search("battle.net")
+.then(result => Connectivity.tryResults(result.data.items!))
+.then(result => console.log(result));
