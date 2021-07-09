58ffe51416400c7830e602a891a9c6fbc4dc481d
SeitaHigashi <higashi110902@gmail.com>
2021-07-09 19:29:29 +0900

    Update app and index.html

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express from "express"
+import {Search} from "./search"
+import {Connectivity} from "./connectivity"
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.post("/",async (req, res) => {
+  res.header('Content-Type', 'text/html;charset=utf-8')
+  console.log(req.body.words);
+  Search.search(req.body.words)
+  .then(results => Connectivity.tryResults(results.data.items!))
+  .then(results => res.end(results.toString()));
+});
+
 app.get("/", (_, res) => {
+  res.header('Content-Type', 'text/html;charset=utf-8')
   res.sendFile('index.html', {root: 'web'});
 });
 
 app.get("/:file", (req, res) => {
   const file = req.params.file;
+  res.header('Content-Type', 'text/html;charset=utf-8')
   res.sendFile(`${file}`,{ root: 'web' });
 });
 
