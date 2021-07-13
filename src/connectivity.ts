e02c443f8ec4440c8239cf35c4a352c14aad7d0c
SeitaHigashi <higashi110902@gmail.com>
2021-07-13 13:21:57 +0900

    Update connectivity

--- src/connectivity.ts
+++ src/connectivity.ts
@@ -1,13 +1,16 @@
 import {customsearch_v1} from "googleapis"
 import {StoreSet} from "./storeSet"
-global.fetch = require('node-fetch').default;
+import {FetchError} from "node-fetch"
+global.fetch = require('node-fetch-with-proxy');
 
 export class Connectivity {
   public static async try(url:string) {
     try {
       const response = await fetch(url);
       return response.ok;
-    } catch (error) {
+    } catch (error)  {
+      if(!(error instanceof FetchError))
+        console.log(error);
       return false;
     }
   }
