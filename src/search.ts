61a41413b0e17d2bb71bd24cd8f6da3ed2bed5a9
SeitaHigashi <higashi110902@gmail.com>
2021-07-14 15:23:38 +0900

    Update search
    number of results > 10

--- src/search.ts
+++ src/search.ts
@@ -1,15 +1,24 @@
-import {google} from "googleapis"
+import {customsearch_v1, google} from "googleapis"
 
 export class Search {
-  public static async search(keywords:string) {
+  public static async search(keywords:string, num:number=10) {
     const customSearch = google.customsearch('v1');
-    let result = await customSearch.cse.list({
-      auth: process.env.GOOGLE_API_KEY,
-      cx: process.env.GOOGLE_CSE_ID,
-      q: keywords,
-      lr: 'lang_ja',
-    });
-    return result.data.items;
+    const params = new Array<customsearch_v1.Params$Resource$Cse$List>();
+    for(let i = 0; i < num/10; i++) {
+      params.push({
+        auth: process.env.GOOGLE_API_KEY,
+        cx: process.env.GOOGLE_CSE_ID,
+        q: keywords,
+        lr: 'lang_ja',
+      });
+    }
+    return Promise.all(
+      params.map(async param => await customSearch.cse.list(param))
+    ).then(result => {
+      return result.map(result => result.data.items!)
+    }).then(result =>
+    result.reduce((acc, val) => acc.concat(val), [])
+    );
   }
 }
 
